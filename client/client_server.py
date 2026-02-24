9e8227d76e6eb24e42dee6eb7383089f013c4b5d
zhouanqi <angiechou1231@gmail.com>
2026-02-24 02:55:57 +0200

    flask debug off

--- client/client_server.py
+++ client/client_server.py
@@ -52,4 +52,4 @@ if __name__ == "__main__":
     observer.start()
 
     # start flask interface ###############################################
-    routes.app.run(host="0.0.0.0", port=5000, debug=True)
+    routes.app.run(host="0.0.0.0", port=5000)
